e308dba8e0ae0bbdaf9ef6f21e98948dfe7adabc
Mark Tutkowski <tutkows1@gmail.com>
2023-02-27 20:30:23 -0600

    Create graphql endpoint

---
+++ api/graphql/index.ts
@@ -0,0 +1,47 @@
+import * as fs from 'fs';
+import { ApolloServer, gql } from 'apollo-server-azure-functions';
+import { OpenWeatherClient } from './open-weather-client';
+
+// Schema definition.
+const schema = fs.readFileSync('./schema.graphql').toString();
+const typeDefs = gql(schema);
+
+// Resolver map.
+const resolvers = {
+    Query: {
+        currentWeather: async (_: any, args: { latitude: number, longitude: number }) => {
+            const client = new OpenWeatherClient();
+            const result = await client.fetchCurrentWeather(args.latitude, args.longitude);
+            return {
+                timestamp: new Date(result.dt * 1000).toISOString(),
+                iconUrl: `http://openweathermap.org/img/wn/${result.weather[0].icon}@2x.png`,
+                weatherType: result.weather[0].main,
+                description: result.weather[0].description,
+                actualTemperature: result.main.temp,
+                feelsLikeTemperature: result.main.feels_like,
+            };
+        },
+        forecast: async (_: any, args: { latitude: number, longitude: number }) => {
+            const client = new OpenWeatherClient();
+            const result = await client.fetchWeatherForecast(args.latitude, args.longitude);
+            return result.list.map(weatherData => ({
+                timestamp: new Date(weatherData.dt * 1000).toISOString(),
+                iconUrl: `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`,
+                weatherType: weatherData.weather[0].main,
+                description: weatherData.weather[0].description,
+                actualTemperature: weatherData.main.temp,
+                feelsLikeTemperature: weatherData.main.feels_like,
+            }));
+        },
+    },
+};
+
+// Create our server.
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    csrfPrevention: true,
+    cache: 'bounded',
+});
+
+export default server.createHandler();
---
+++ api/graphql/open-weather-client.ts
@@ -0,0 +1,58 @@
+import axios from 'axios';
+
+export interface WeatherData {
+  dt: number;
+  coord: {
+    lon: number;
+    lat: number;
+  },
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+    sea_level: number;
+    grnd_level: number;
+  };
+  visibility: number;
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  rain: {
+    "1h": number;
+  };
+  clouds: {
+    all: number;
+  };
+  timezone: number;
+  name: string;
+}
+
+export interface ForecastData {
+  list: WeatherData[];
+}
+
+export class OpenWeatherClient {
+  private readonly apiKey: string = process.env.OPEN_WEATHER_API_KEY || '';
+  private readonly units: string = 'imperial';
+
+  public async fetchCurrentWeather(latitude: number, longitude: number): Promise<WeatherData> {
+    const result = await axios.get<WeatherData>(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&unts=${this.units}&appid=${this.apiKey}`);
+    return result.data;
+  }
+
+  public async fetchWeatherForecast(latitude: number, longitude: number): Promise<ForecastData> {
+    const result = await axios.get<ForecastData>(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&unts=${this.units}&appid=${this.apiKey}`);
+    return result.data;
+  }
+}
